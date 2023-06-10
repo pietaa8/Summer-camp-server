65a1a5a9724a7064046752c0c78dc6db24bc353e
priyab99 <priya.bd.9911@gmail.com>
2023-06-10 22:31:18 +0600

    users getting

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const cors = require('cors');
-require('dotenv').config()
-const port=process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3cfwic.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,23 +23,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+     
+    const usersCollection = client.db('sportsDB').collection('users');
+
+    // Users related API
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-  client.close();
+    //client.close();
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
-})
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
